1da223a8af6a57e5ec21f5c35aab97f9502c4841
Dimitris <jim600v@gmail.com>
2025-11-08 06:32:02 +0200

    Implement image upload feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -80,7 +80,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
 
 class RecipeImageSerializer(serializers.ModelSerializer):
     """Serializer for uploading images to recipes."""
